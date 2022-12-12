874f3f9245b33985377ce71c04753725b4856b78
Jasonwtw <jasonwtw@outlook.com>
2022-12-12 11:13:55 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='demo01',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='tingwei.wu',
+    author_email='',
+    description=''
+)
